2dd1b3c7c7ada7f1f82e82f8b19bdf56098197e2
joesid <joesidious@gmail.com>
2023-09-19 12:49:52 +0100

    First Commit - Initialize Project

---
+++ src/main/java/com/example/chat_room_springboot/ChatRoomSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.chat_room_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRoomSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chat_room_springboot/ChatRoomSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_room_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRoomSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
